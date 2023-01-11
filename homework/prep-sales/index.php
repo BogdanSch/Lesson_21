dfe1008467a0666396c27f6f9b278f11628a6e45
Bohdan <bogmira777@gmail.com>
2023-01-11 21:51:07 +0100

    Fix prepeared expressions task

--- homework/prep-sales/index.php
+++ homework/prep-sales/index.php
@@ -5,25 +5,35 @@ include("../page-parts/action.php");
 
 //1
 $stmt = $conn->prepare("SELECT * FROM products_sales WHERE product_sales=(SELECT MAX(?) FROM products_sales);");
-$stmt->bind_param("s", $sales);
-$sales = "product_sales";
-$result = $stmt->execute();
+$stmt->bind_param("s", $val);
+$val = "50";
+$stmt->execute();
+$result = ($stmt->get_result());
 print_array($result);
 echo "<hr>";
 //2
-$sql = "SELECT SUM(product_price) AS TOTAL_PRICES FROM products_sales;";
-$result = $conn->query($sql);
-echo($result->fetch_assoc()['TOTAL_PRICES']);
+$stmt = $conn->prepare("SELECT SUM(?) AS TOTAL_PRICES FROM products_sales;");
+$stmt->bind_param("s", $search_key);
+$search_key = "product_price";
+$stmt->execute();
+$result = ($stmt->get_result());
+echo($result->fetch_assoc()["TOTAL_PRICES"]);
 echo "<hr>";
 //3
-$sql = "SELECT * FROM products_sales ORDER BY product_name DESC;";
-$result = $conn->query($sql);
+$stmt = $conn->prepare("SELECT * FROM products_sales ORDER BY ? DESC;");
+$stmt->bind_param("s", $order_key);
+$order_key = "product_name";
+$stmt->execute();
+$result = ($stmt->get_result());
 print_array($result);
 echo "<hr>";
 //4
-$sql = "SELECT * FROM products_sales WHERE product_sales > 20
-AND (product_sales > 15 AND product_sales < 500);";
-$result = $conn->query($sql);
+$stmt = $conn->prepare("SELECT * FROM products_sales WHERE product_sales > ?
+AND (product_sales > 15 AND product_sales < 500);");
+$stmt->bind_param("s", $val);
+$val = "20";
+$stmt->execute();
+$result = ($stmt->get_result());
 print_array($result);
 
 $conn->close();
